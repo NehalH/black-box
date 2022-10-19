b26eaf24a3a491e598d93f40b190d0d7851070d9
NehalH <nhosalikar@gmail.com>
2022-10-19 21:31:08 +0530

    Stores readings in SDcard

--- firmware_dev/firmware_dev.ino
+++ firmware_dev/firmware_dev.ino
@@ -1,18 +1,25 @@
-#include <Wire.h>
+#include <Wire.h> // imports the wire library for talking over I2C 
 
-const int MPU_addr=0x68;
+#include <SD.h> //Load SD card library
+#include<SPI.h> //Load SPI Library
+
+const int MPU_addr=0x68; 
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
- 
+
 int minVal=265;
 int maxVal=402;
- 
+
 int x;
-int y;
-int z;
+
+int chipSelect = 4; //chipSelect pin for the SD card Reader
+File mySensorData; //Data object you will write your sesnor data to
 
 void setup() {
   
   Serial.begin(9600);
+
+  pinMode(10, OUTPUT); //Must declare 10 an output and reserve it
+  SD.begin(4); //Initialize the SD card reader
   
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
@@ -25,7 +32,9 @@ void setup() {
 /////////////////////////////////////////////////////////////////
 
 void loop() { 
- 
+  
+  mySensorData = SD.open("PTData.txt", FILE_WRITE);
+  
   // Read angle
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B);
@@ -45,13 +54,17 @@ void loop() {
   //z= RAD_TO_DEG * (atan2(-yAng, -xAng)+PI);
   
   if(x>90) x-=360;                                          // Convert angles (x>180) to Negative angles (0 to -180)
-  if(y>90) y-=360;  
- 
+  //if(y>90) y-=360;  
 
-  Serial.println("\n-----------------------------------------\n");
-  Serial.print("AngleX= ");
-  Serial.println(x);
- 
+  if (mySensorData) {
+    Serial.println("\n-----------------------------------------\n");
+    Serial.print("AngleX= ");
+    Serial.println(x);
+
+    mySensorData.print(x);
+    mySensorData.close();
+    
+  }
   delay(1000);
 
 }
