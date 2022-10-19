6e39c77e851887bd4b21c64bd5459a7b827d1225
NehalH <nhosalikar@gmail.com>
2022-10-19 18:48:08 +0530

    Boiler plate for mpu

---
+++ blackBox.ino
@@ -0,0 +1,78 @@
+#include<Wire.h>
+#include <SPI.h>
+#include <SD.h>
+//#include <RTClib.h>
+
+RTC_DS3231 rtc;
+File myFile;
+
+const int MPU_addr=0x68;
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+ 
+int minVal=265;
+int maxVal=402;
+ 
+int x;
+int y;
+int z;
+//////////////////////////////////////////////////////////////////////
+void setup(){
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(10)) {
+    Serial.println("initialization failed!");
+    while (1);
+    }
+  Serial.println("initialization done.");
+
+  Serial.begin(9600);
+}
+//////////////////////////////////////////////////////////////////////
+void loop(){
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);
+  AcX=Wire.read()<<8|Wire.read();
+  AcY=Wire.read()<<8|Wire.read();
+  AcZ=Wire.read()<<8|Wire.read();
+  int xAng = map(AcX,minVal,maxVal,-90,90);
+  int yAng = map(AcY,minVal,maxVal,-90,90);
+  int zAng = map(AcZ,minVal,maxVal,-90,90);
+
+  x= RAD_TO_DEG * (atan2(-yAng, -zAng)+PI);
+  y= RAD_TO_DEG * (atan2(-xAng, -zAng)+PI);
+  z= RAD_TO_DEG * (atan2(-yAng, -xAng)+PI);
+
+  Serial.print("AngleX= ");
+  Serial.println(x);
+
+  Serial.print("AngleY= ");
+  Serial.println(y);
+
+  Serial.print("AngleZ= ");
+  Serial.println(z);
+  Serial.println("-----------------------------------------");
+
+  DateTime now = rtc.now();
+  myFile = SD.open("test.txt", FILE_WRITE);
+
+  myFile.println("AngleX=  ");
+  myFile.print(x);
+  myFile.print("\t\t\t\t\t");
+  /*
+  myFile.print(now.hour(), DEC);
+  myFile.print(':');
+  myFile.print(now.minute(), DEC);
+  myFile.print(':');
+  myFile.print(now.second(), DEC);
+  myFile.close();
+  */
+  delay(1000);
+}
+  
